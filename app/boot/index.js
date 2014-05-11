cdaccf363c20745c298804d4f0e7d9ff8ed2bc4f
Bodo Kaiser <i@bodokaiser.io>
2014-05-11 10:59:41 +0200

    restructured app/boot routes setup

--- app/boot/index.js
+++ app/boot/index.js
@@ -1,7 +1,31 @@
-import page from 'page';
+import page  from 'page';
+import auth  from 'auth';
+import shell from 'shell';
 
-import './secure';
-import './reporter';
-import './customers';
+import signin    from './signin';
+import signout   from './signout';
+import reporter  from './reporter';
+import customers from './customers';
+
+page(function(context, next) {
+  if (context.path === '/login') return next();
+
+  auth.check(function(err, active) {
+    if (err) throw err;
+
+    if (!active) {
+      page('/login');
+    } else {
+      shell.navbar.showActions();
+
+      next();
+    }
+  });
+});
+
+page('/signin', signin);
+page('/signout', signout);
+page('/reporter', reporter);
+page('/customers', customers);
 
 page();
