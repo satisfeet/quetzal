d6c3beb1779fdb286b248b96a02a120d34f328da
Bodo Kaiser <i@bodokaiser.io>
2014-05-11 21:28:27 +0200

    moved app/boot to app/index.js

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-import 'boot';
+import page  from 'page';
+import auth  from 'auth';
+import shell from 'shell';
+
+page(function(context, next) {
+  if (context.path === '/login') return next();
+
+  auth.check(function(err, active) {
+    if (err) throw err;
+
+    if (!active) {
+      page('/login');
+    } else {
+      shell.navbar.showActions();
+
+      next();
+    }
+  });
+});
+
+import 'reporter';
+import 'customers';
+
+page();
