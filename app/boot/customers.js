e2cbc6e588b8ca589352cede3a3280f3e5df7db2
Bodo Kaiser <i@bodokaiser.io>
2014-05-11 13:59:20 +0200

    added form to app/customers

--- app/boot/customers.js
+++ app/boot/customers.js
@@ -1,8 +1,12 @@
 import shell     from 'shell';
 import customers from 'customers';
 
-export default = function(context, next) {
+export function list(context, next) {
   customers.manager.find(function(result) {
     shell.layout.empty().append(customers.table.list(result).element);
   });
-};
+}
+
+export function create(context, next) {
+  shell.layout.empty().append(customers.form.element);
+}
--- app/customers/index.js
+++ app/customers/index.js
@@ -1,5 +1,7 @@
+import Form    from './form';
 import Table   from './table';
 import Manager from './manager';
 
+export var form = new Form();
 export var table = new Table();
 export var manager = new Manager();
