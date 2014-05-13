9d0db07ac874e96bf3b9afcf0de33507f52099eb
Bodo Kaiser <i@bodokaiser.io>
2014-05-13 08:54:04 +0200

    added disabled behaviour to navbar and glyphicons

--- app/auth/auth.js
+++ app/auth/auth.js
@@ -5,12 +5,10 @@ import emitter from 'emitter';
 function Auth() {
   emitter(this);
 
-  var self = this;
-
   this.agent = agent('/session');
   this.agent.on('error', function(err) {
-    self.emit('error', err);
-  });
+    this.emit('error', err);
+  }.bind(this));
 }
 
 Auth.prototype.token = function() {
@@ -22,23 +20,21 @@ Auth.prototype.check = function() {
     return this.emit('check', false);
   }
 
-  var self = this;
-
   this.agent.get(function(ok, body) {
-    self.emit('check', ok);
-  });
+    if (!ok) store.set('session', null);
+  
+    this.emit('check', ok);
+  }.bind(this));
 
   return this;
 };
 
 Auth.prototype.signin = function(account) {
-  var self = this;
-
   this.agent.post(account, function(ok, body) {
     if (ok) store.set('session', body.token);
 
-    self.emit('signin', ok);
-  });
+    this.emit('signin', ok);
+  }.bind(this));
 
   return this;
 };
--- app/navbar/navbar.js
+++ app/navbar/navbar.js
@@ -6,10 +6,30 @@ function Navbar() {
 
   this.events = events(this.element, this);
   this.events.bind('click .dropdown-toggle', 'toggleActions');
+  this.events.bind('click .dropdown-menu li > a', 'toggleActions');
 }
 
-Navbar.prototype.toggleActions = function() {
-  if (this.active) query('.dropdown', this.element).classList.toggle('open');
+Navbar.prototype.toggleActions = function(e) {
+  // we only want to prevent the default event on .dropdown-toggle
+  if (e && e.target.className) e.preventDefault();
+
+  if (this.active) {
+    query('.dropdown', this.element).classList.toggle('open');
+  }
+
+  return this;
+};
+
+Navbar.prototype.toggleActionsState = function(active) {
+  this.active = active;
+
+  var element = query('.dropdown', this.element);
+
+  if (!this.active) {
+    element.classList.add('disabled');
+  } else {
+    element.classList.remove('disabled');
+  }
 
   return this;
 };
