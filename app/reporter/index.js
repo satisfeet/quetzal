6e10236ff7a85f2719b62c4e01949cbd94cdc162
Bodo Kaiser <i@bodokaiser.io>
2014-05-13 22:06:59 +0200

    removed domify dependency from app/reporter

--- app/reporter/index.js
+++ app/reporter/index.js
@@ -1,12 +1,11 @@
 import page   from 'page';
 import agent  from 'agent';
-import domify from 'domify';
 import layout from 'layout';
 
 import template from './template';
 
 page('/', function() {
   agent().get('/', function(ok, body) {
-    layout.content.empty().append(domify(template(body)));
+    layout.content.element.innerHTML = template(body);
   });
 });
