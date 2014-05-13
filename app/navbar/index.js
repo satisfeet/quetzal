b45ed3ebfcf72209e791ee98498f98cead112a0b
Bodo Kaiser <i@bodokaiser.io>
2014-05-13 08:54:04 +0200

    added disabled behaviour to navbar and glyphicons

--- app/navbar/index.js
+++ app/navbar/index.js
@@ -4,10 +4,13 @@ import Navbar from './navbar';
 
 var navbar = new Navbar();
 
+transform(auth.token());
+
 auth.on('check', transform);
 auth.on('signin', transform);
 auth.on('signout', transform);
 
 function transform(success) {
-  navbar.active = success;
+  console.log('transform');
+  navbar.toggleActionsState(success);
 }
