30540625cf6e7e74313fbbe64df215b14653979b
Bodo Kaiser <i@bodokaiser.io>
2014-05-14 11:30:53 +0200

    added contenteditable to app/customers/section

---
+++ app/customers/section/index.js
@@ -0,0 +1,64 @@
+import clone   from 'clone';
+import domify  from 'domify';
+import emitter from 'emitter';
+
+import section from './section';
+
+function Section(model) {
+  // do not overwrite the source model!
+  model = clone(model);
+
+  this.element = domify(section(model));
+
+  bindToInputEvent(this.element, model, this);
+  bindToButtonClickEvents(this.element, model, this);
+}
+
+emitter(Section.prototype);
+
+Section.prototype.alert = function(message) {
+  var element = this.element.querySelector('.alert');
+
+  element.classList.remove('hidden');
+  element.innerText = message;
+
+  return this;
+};
+
+Section.prototype.toggleActions = function() {
+  if (!this.toggledActions) {
+    this.element.querySelector('#actions').classList.toggle('hidden');
+    this.element.querySelector('#controls').classList.toggle('hidden');
+  }
+
+  return this;
+};
+
+export default Section;
+
+function bindToInputEvent(element, model, view) {
+  element.addEventListener('input', function(e) {
+    if (~['street', 'city', 'zip'].indexOf(e.target.id)) {
+      model.address[e.target.id] = e.target.textContent;
+    } else {
+      model[e.target.id] = e.target.textContent;
+    }
+
+    view.toggleActions();
+    view.toggledActions = true;
+  });
+}
+
+function bindToButtonClickEvents(element, model, view) {
+  element.querySelector('button.btn-danger')
+    .addEventListener('click', function(e) {
+      view.emit('submit', model);
+
+      view.toggledActions = false;
+      view.toggleActions();
+    });
+  element.querySelector('button.btn-default')
+    .addEventListener('click', function(e) {
+      view.emit('reset', model);
+    });
+}
--- app/customers/show/index.js
+++
@@ -1,9 +0,0 @@
-import domify  from 'domify';
-
-import section from './section';
-
-function Show(model) {
-  this.element = domify(section(model));
-}
-
-export default Show;
