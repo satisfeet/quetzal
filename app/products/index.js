eaaefb1ddf18501264a86b7ecf830f0355bcbd9b
Bodo Kaiser <i@bodokaiser.io>
2014-05-15 09:27:14 +0200

    implemented app/products/list

--- app/products/index.js
+++ app/products/index.js
@@ -1,6 +1,17 @@
 import page   from 'page';
+import agent  from 'agent';
 import layout from 'layout';
 
+import List from './list';
+
+var manager = agent('/products');
+
 page('/products', function(context, next) {
-  layout.content.element.innerHTML = '<p>products</p>';
+  var list = new List();
+
+  manager.get(function(ok, body, state) {
+    list.list(body);
+  });
+
+  layout.content.empty().append(list.element);
 });
