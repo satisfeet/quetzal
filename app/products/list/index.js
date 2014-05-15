14a6cf788fe679179fe465cf94bd27c111128365
Bodo Kaiser <i@bodokaiser.io>
2014-05-15 09:27:14 +0200

    implemented app/products/list

---
+++ app/products/list/index.js
@@ -0,0 +1,30 @@
+import domify from 'domify';
+
+import item from './item';
+import list from './list';
+
+function Item(model) {
+  this.element = domify(item({
+    product: model
+  }));
+}
+
+function List(models) {
+  this.element = domify(list({
+    products: models
+  }));
+}
+
+List.prototype.add = function(model) {
+  this.element.appendChild(new Item(model).element);
+
+  return this;
+};
+
+List.prototype.list = function(models) {
+  models.forEach(this.add, this);
+
+  return this;
+};
+
+export default List;
