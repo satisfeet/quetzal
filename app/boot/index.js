00803237e0a28f4a559d59c4789dcf7a42492eac
Bodo Kaiser <i@bodokaiser.io>
2014-06-13 17:41:21 +0200

    replaced JWT based auth with HTTP Basic

--- app/boot/index.js
+++ app/boot/index.js
@@ -1,24 +1,19 @@
-var auth   = require('auth');
 var page   = require('page');
 var query  = require('query');
 var modal  = require('modal');
 var rester = require('rester');
 
-rester.use(bearer);
+rester.use(accept);
 
 require('./modal');
 
-require('auth');
-require('navbar');
-
 require('home');
+require('navbar');
 require('products');
 require('customers');
 
 page();
 
-function bearer(request) {
-  var token = auth.token();
-
-  if (token) request.set('Authorization', 'Bearer ' + token);
+function accept(request) {
+  request.accept('json');
 }
--- app/navbar/index.js
+++ app/navbar/index.js
@@ -1,16 +1,3 @@
-var auth = require('auth');
-
 var Navbar = require('./navbar');
 
 var navbar = new Navbar();
-
-transform(auth.token());
-
-auth.on('check', transform);
-auth.on('signin', transform);
-auth.on('signout', transform);
-
-function transform(success) {
-  navbar.setActions(auth.user());
-  navbar.toggleActionsState(success);
-}
