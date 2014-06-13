a0a92f51bd106cfcfc51ab9c29ace1d0a87c3910
Bodo Kaiser <i@bodokaiser.io>
2014-06-13 17:41:21 +0200

    replaced JWT based auth with HTTP Basic

--- app/products/index.js
+++ app/products/index.js
@@ -8,7 +8,7 @@ var Form   = require('./form');
 var Show   = require('./show');
 var Layout = require('./layout');
 
-var agent = rester('https://engine.satisfeet.me/products');
+var agent = rester('/products');
 
 page('/products', find, function(context) {
   var list = new List(context.products);
