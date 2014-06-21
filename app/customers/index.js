344611ee2d989da6b74c65c733a82cb0d0b0869a
Bodo Kaiser <i@bodokaiser.io>
2014-06-21 11:32:33 +0200

    created basic setup for /app/customers

--- app/customers/index.js
+++ app/customers/index.js
@@ -1,1 +1,44 @@
-console.log('boot');
+var List     = require('list');
+var Show     = require('show');
+var Layout   = require('layout');
+var Router   = require('router');
+var Customer = require('customer');
+
+var router = new Router();
+var layout = new Layout();
+
+router.on('/customers', find, function(context) {
+  var list = new List(context.customers);
+
+  list.once('click', function(id) {
+    router.go('/customers/' + id);
+  });
+
+  layout.insert(list.element);
+});
+
+router.on('/customers/:customer', findOne, function(context) {
+  var show = new Show(context.customer);
+
+  layout.insert(show.element);
+});
+
+router.start();
+
+function find(context, next) {
+  Customer.all(function(err, collection) {
+    context.customers = collection;
+
+    next();
+  });
+}
+
+function findOne(context, next) {
+  var param = context.params.customer;
+
+  Customer.get(param, function(err, model) {
+    context.customer = model;
+
+    next();
+  });
+}
